973c943567d480b61fcd5a60d39d308e0b93ddcd
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-17 10:05:10 +0100

    first commit

--- src/components/01-useState/CounterWithCustomHook.js
+++ src/components/01-useState/CounterWithCustomHook.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useCounter } from '../../hooks/useCounter';
+import './counter.css';
 
 export const CounterWithCustomHook = () => {
+
+    const { state, increment, decrement, reset } = useCounter(0);
+
     return (
         <>
-            <h1>CounterHook: {0} </h1>
+            <h1>CounterHook: {state} </h1>
             <hr/>
 
-            <button className="btn"> +1 </button>
-            <button className="btn"> -1 </button>
+            <button onClick={increment} className="btn"> +1 </button>
+            <button onClick={decrement} className="btn"> -1 </button>
+            <button onClick={reset} className="btn"> RESET </button>
         </>
     )
 }
