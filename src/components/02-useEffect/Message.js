7e88ffdf93ba267b6af38f2345b59f6ec653366f
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-20 10:35:13 +0100

    first commit

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,23 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Message = () => {
 
-    useEffect(() => {
+    const [coords, setCoords] = useState({x:0, y:0})
+    const {x, y} = coords;   
 
-        window.addEventListener('mousemove', (e)=>{
-            // console.log(e);
-            const coors = {x: e.x, y: e.y};
-            console.log(coors)
-        })
+    useEffect(() => {     
+
+        const mouseMove = (e) =>{
+            const coords = {x: e.x, y: e.y};
+            setCoords( coords );
+        }
+
+        window.addEventListener('mousemove', mouseMove)
 
         return () => {
-            console.log('component unmounted!')
+            window.removeEventListener('mousemove', mouseMove)
         }
     }, [])
 
     return (
         <div>
             <h3>You're great!</h3>
+            <p>
+                x:{ x } y: { y }
+            </p>
         </div>
     )
 }
