53cc4228c88524f09deaf993d1d6395a2eac4c7b
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-21 17:56:29 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterApp } from './components/01-useState/CounterApp';
 // import { HookApp } from './HookApp';
 // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
 ReactDOM.render(
-  <SimpleForm />,
+  <FormWithCustomHook />,
   document.getElementById('root')
 );
 
