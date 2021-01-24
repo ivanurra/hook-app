634fbd5b54b61429f2e9cbdb5ae688a049f9454a
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-24 08:14:11 +0100

    useRef

---
+++ src/components/04-useRef/FocusScreen.js
@@ -0,0 +1,29 @@
+import React, { useRef } from 'react'
+import '../02-useEffect/simple-effect.css';
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef()
+
+    const handleClick = () => {
+        inputRef.current.select();
+    }
+
+    return (
+        <div>
+            <h1>Focus Screen</h1>
+            <hr />
+            <input
+                ref= { inputRef } 
+                className="form-control"
+                placeholder="Your name"
+            />
+            <button 
+                className="btn btn-outline-primary mt-2"
+                onClick={ handleClick }
+            >
+                FOCUS
+            </button>
+        </div>
+    )
+}
