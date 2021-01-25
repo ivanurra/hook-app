d5c98f0a243d9a5063b307dcf530e62bd64203cb
Ivan Urra <ivan.urra.wd@gmail.com>
2021-01-25 09:15:00 +0100

    RealExampleRef

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const useFetch = (url) => {
    
+    const isMounted = useRef(true);
+
     const [state, setState] = useState({ data: null, loading: true, error: null })
+
+    useEffect(() => {
+       return () => {
+           isMounted.current = false;
+       }
+    }, [])
 
     useEffect (() => {
 
@@ -12,11 +20,18 @@ export const useFetch = (url) => {
         .then (resp => resp.json())
         .then (data => {
 
-            setState({
-                loading: false,
-                error: null,
-                data
-            })
+            setTimeout(() => {
+
+                if (isMounted.current) {
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+                    });
+                } else {
+                    console.log('setState was not called');
+                }
+            }, 4000);
 
         })
 
