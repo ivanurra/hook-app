081d4d9d054ada017e56bd111c9002194e257792
Ivan Urra <ivan.urra.wd@gmail.com>
2021-02-02 11:49:11 +0100

    todo app

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -7,6 +7,12 @@ export const TodoApp = () => {
             <h1>TodoApp</h1>
             <hr/>
 
+            <ul>
+                <li>Tarea1</li>
+                <li>Tarea2</li>
+                <li>Tarea3</li>
+            </ul>
+
         </div>
     )
 }
